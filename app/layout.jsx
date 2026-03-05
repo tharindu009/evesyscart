2e7a854b6101ce64324f7224f745ba150a556280
Harendra <tharindu009@hotmail.com>
2026-03-04 20:27:49 -0600

    update title

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,8 +7,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export const metadata = {
-    title: "GoCart. - Shop smarter",
-    description: "GoCart. - Shop smarter",
+    title: "nextmart - Shop smarter",
+    description: "nextmart - Shop smarter",
 };
 
 export default function RootLayout({ children }) {
